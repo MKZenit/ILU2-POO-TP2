3c8e1681311bc328dacc57339aea0aa45e4e7397
MKZenit <enzodanige@gmail.com>
2024-03-13 11:59:46 +0100

    Modification TP2-4

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -27,7 +27,12 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					this.controlEmmenager.ajouterGaulois(nomVisiteur, 
+							Clavier.entrerEntier(new StringBuilder()
+							.append("Bienvenue villageois ")
+							.append(nomVisiteur)
+							.append("\nQuelle est votre force ?\n")
+						.toString()) );
 					break;
 
 				default:
@@ -40,6 +45,20 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+		int forceDruide = Clavier.entrerEntier(new StringBuilder()
+				.append("Bienvenue druide ")
+				.append(nomVisiteur)
+				.append("\nQuelle est votre force ?\n")
+				.toString( ));
+		int effetPotionMax, effetPotionMin;
+		do {
+			effetPotionMin = Clavier.entrerEntier(
+					"Quelle est la force de potion la plus faible que vous produisez ? ");
+			effetPotionMax = Clavier.entrerEntier(
+					"Quelle est la force de potion la plus forte que vous produisez ? ");
+		if(effetPotionMax<effetPotionMin) System.out.println("Attention Druide, vous vous êtes trompé entre le minimum et le maximum\n");
+		}while(effetPotionMax<effetPotionMin);
+		
+		this.controlEmmenager.ajouterDruide(nomVisiteur, forceDruide, effetPotionMin, effetPotionMax);
 	}
 }
--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -13,10 +13,51 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		boolean nomVendeurConnu = this.controlPrendreEtal.verifierIdentite(nomVendeur);
+		if(!nomVendeurConnu) {
+			System.out.println(new StringBuilder()
+					.append("Je suis désolée ")
+					.append(nomVendeur)
+					.append(" mais il faut être un habitant de notre village pour commercer ici.")
+					.toString() );
+		}
+		else {
+			System.out.println(new StringBuilder()
+					.append("Bonjour ")
+					.append(nomVendeur)
+					.append(" ,je vais regarder si je peux vous trouver un étal.")
+					.toString() );
+			if(!this.controlPrendreEtal.resteEtals()) {
+				System.out.println(new StringBuilder()
+						.append("Désolée ")
+						.append(nomVendeur)
+						.append(" je n'ai plus d'étal qui ne soit pas déjà occupé.")
+						.toString() );
+			}
+			else {
+				this.installerVendeur(nomVendeur);
+			}
+			
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		String produit;
+		try (Scanner clavier = new Scanner(System.in)) {
+			System.out.println("C'est parfait, il me reste un étal pour vous !\nIl me faudrait quelques reseignements :\n");
+			System.out.println("Quel produit souhaitez-vous vendre ?");
+			produit = clavier.nextLine();
+
+		}
+			int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre ?");
+			int numeroEtal = this.controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+			if(numeroEtal != -1) {
+				System.out.println(new StringBuilder()
+						.append("Le vendeur ")
+						.append(nomVendeur)
+						.append(" s'est installé à l'étal n°")
+						.append(numeroEtal)
+						.toString() );
+			}
 	}
 }
