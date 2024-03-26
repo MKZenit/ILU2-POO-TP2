233793fc3b0bd69f612b47e6afed6579c7855aa4
MekaZenit <enzodanige@gmail.com>
2024-03-26 23:55:48 +0100

    TP2_3 Cas 8 presque terminé

--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -3,23 +3,25 @@ package frontiere;
 import java.util.Scanner;
 
 import controleur.ControlAcheterProduit;
+import controleur.ControlAfficherMarche;
 
 public class BoundaryAcheterProduit {
 	private Scanner scan = new Scanner(System.in);
 	private ControlAcheterProduit controlAcheterProduit;
+	private ControlAfficherMarche controlAfficherMarche;
 
 	public BoundaryAcheterProduit(ControlAcheterProduit controlAcheterProduit) {
 		this.controlAcheterProduit = controlAcheterProduit;
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		if(this.controlAcheterProduit.verifierIdentite(nomAcheteur)) {
-			System.out.println("Je suis désolée Bonemine mais il faut être un habitant de notre village pour commercer ici.");
+		if(!this.controlAcheterProduit.verifierIdentite(nomAcheteur)) {
+			System.out.println("Je suis dï¿½solï¿½e " + nomAcheteur + " mais il faut ï¿½tre un habitant de notre village pour commercer ici.");
 		}
 		else {
 			StringBuilder question = new StringBuilder();
 			question.append("1 - je veux acheter un produit.\n");
-			question.append("2 - je veux avoir une vue d'ensemble du marché.\n");
+			question.append("2 - je veux avoir une vue d'ensemble du marchï¿½.\n");
 			question.append("3 - quitter l'application.\n");
 			int choixUtilisateur = -1;
 			do {
@@ -29,24 +31,64 @@ public class BoundaryAcheterProduit {
 					System.out.println("Quel produit voulez-vous acheter ?");
 					String produit = scan.nextLine();
 					question = new StringBuilder()
-							.append("Chez quel commerçant voulez-vous acheter des fleurs ?");
+							.append("Chez quel commerï¿½ant voulez-vous acheter des fleurs ?");
 					String[] vendeursProduit = this.controlAcheterProduit.trouverVendeursProduit(produit);
 					if(vendeursProduit.length==0) {
-						System.out.println("Désolé, personne ne vend ce produit au marché.");
+						System.out.println("Dï¿½solï¿½, personne ne vend ce produit au marchï¿½.");
 					}
 					else {
+						question.append("\n");
 						for(int i=0; i<vendeursProduit.length; i++) {
+							question.append(i+"- ");
 							question.append(vendeursProduit[i]);
+							question.append("\n");
 						}
-						int choixVendeur = Clavier.entrerEntier(question.toString());
-						// TODO vérifier choixVendeur compris dans les bornes de la liste
+						int choixVendeur = 1;
+						do {
+							if(choixVendeur<1 || choixVendeur>vendeursProduit.length) 
+								System.out.println("Vous devez entrer un nombre compris entre 1 et "+vendeursProduit.length);
+							choixVendeur = Clavier.entrerEntier(question.toString());
+						}while(choixVendeur<1 || choixVendeur>vendeursProduit.length);
 						String nomVendeurChoisi = vendeursProduit[choixVendeur];
-						// TODO suite cas 2
+						System.out.println(nomAcheteur+" se dÃ©place jusqu'Ã  l'Ã©tal du vendeur "+nomVendeurChoisi);
+						var etal = this.controlAcheterProduit.trouverEtalVendeur(nomVendeurChoisi);
+						System.out.println("Bonjour "+nomAcheteur);
+						int nbSouhaite = Clavier.entrerEntier("Combien de "+produit+ " voulez-vous acheter ?");
+						if(etal.getQuantite()<=0) {
+							System.out.println(nomAcheteur+" veut acheter "+ nbSouhaite + " "+ produit + ", malheureusement il n'y en a plus !");
+						}else if(etal.getQuantite()<nbSouhaite) {
+							System.out.println(new StringBuilder()
+									.append(nomAcheteur)
+									.append(" veut acheter ")
+									.append(nbSouhaite)
+									.append(" ")
+									.append(produit)
+									.append(", malheureusement ")
+									.append(nomVendeurChoisi)
+									.append(" n'en a plus que ")
+									.append(etal.getQuantite())
+									.append(". ")
+									.append(nomAcheteur)
+									.append(" achÃ¨te tout le stock de Bonemine.")
+									.toString());
+							etal.acheterProduit(etal.getQuantite());
+						}
+						else {
+							System.out.println(new StringBuilder()
+									.append(nomAcheteur)
+									.append(" achÃ¨te ")
+									.append(nbSouhaite)
+									.append(produit)
+									.append(" Ã  ")
+									.append(nomVendeurChoisi)
+									.toString());
+							etal.acheterProduit(nbSouhaite);
+						}
 					}
 					break;
 
 				case 2:
-					//TODO cas 2
+					this.controlAfficherMarche.donnerInfosMarche();
 					break;
 
 				default:
